66613abea30a93d17e8bbe51ae899c949d1c6b6b
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-07-25 18:27:10 +0900

    Wip test

--- __tests__/wip.test.js
+++ __tests__/wip.test.js
@@ -1,9 +1,9 @@
 import { createTransport } from "../lib";
 
-// jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60;
 
 describe("simple-p2p", () => {
-  it("should work", async () => {
+  it("should open peer", async done => {
     const t1 = createTransport();
     const t2 = createTransport({
       iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
@@ -22,17 +22,84 @@ describe("simple-p2p", () => {
     // await new Promise(r => setTimeout(r, 1000 * 30));
 
     await Promise.all([
-      new Promise(r =>
-        t1.on("connectionStateChange", s => s === "connected" && r())
-      ),
-      new Promise(r =>
-        t2.on("connectionStateChange", s => s === "connected" && r())
-      )
+      new Promise(r => t1.once("open", r)),
+      new Promise(r => t2.once("open", r))
+    ]);
+    done();
+  });
+
+  it("should send media", async done => {
+    const t1 = createTransport();
+    const t2 = createTransport({});
+    t1.on("negotiation", p => t2.handleNegotiation(p).catch(console.error));
+    t2.on("negotiation", p => t1.handleNegotiation(p).catch(console.error));
+    await t1.startNegotiation().catch(console.error);
+    const m1 = t1.mediaHandler;
+    const m2 = t2.mediaHandler;
+
+    await Promise.all([
+      new Promise(r => m1.once("open", r)),
+      new Promise(r => m2.once("open", r))
     ]);
 
-    const p1 = t1.peer;
-    const p2 = t2.peer;
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    });
+    const [vTrack] = stream.getVideoTracks();
+    const [aTrack] = stream.getAudioTracks();
 
-    console.warn(p1, p2);
+    let m2count = 0;
+    m2.on("media", t1 => {
+      console.warn(t1);
+      m2count++;
+    });
+    await m1.sendMedia(vTrack).catch(console.error);
+
+    await new Promise(r => setTimeout(r, 1000 * 3));
+
+    await m1.sendMedia(aTrack).catch(console.error);
+
+    await new Promise(r => setTimeout(r, 1000 * 3));
+
+    expect(m2count).toBe(2);
+
+    m1.on("media", t2 => {
+      expect(t2.kind).toBe("audio");
+      done();
+    });
+    await m2.sendMedia(aTrack.clone()).catch(console.error);
+  });
+
+  fit("should send data", async done => {
+    const t1 = createTransport();
+    const t2 = createTransport({});
+    t1.on("negotiation", p => t2.handleNegotiation(p).catch(console.error));
+    t2.on("negotiation", p => t1.handleNegotiation(p).catch(console.error));
+    await t1.startNegotiation().catch(console.error);
+    const d1 = t1.dataHandler;
+    const d2 = t2.dataHandler;
+
+    await Promise.all([
+      new Promise(r => d1.once("open", r)),
+      new Promise(r => d2.once("open", r))
+    ]);
+
+    d2.on("data", c2 => {
+      console.warn(c2);
+    });
+    const c1 = await d1.createDataChannel().catch(console.error);
+    console.warn(c1);
+
+    await new Promise(r => setTimeout(r, 1000 * 3));
+
+    d1.on("data", c1 => {
+      console.warn(c1);
+      done();
+    });
+    const c2 = await d2
+      .createDataChannel("foo", { ordered: false })
+      .catch(console.error);
+    console.warn(c2);
   });
 });
