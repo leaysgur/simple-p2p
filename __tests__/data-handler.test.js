4cd4bf2a7c2a5d97dd47e6a59bf1136577080bb6
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-07-26 22:33:28 +0900

    Test data-handler

---
+++ __tests__/data-handler.test.js
@@ -0,0 +1,77 @@
+import { createTransport } from "../lib";
+
+let t1;
+let t2;
+let d1;
+let d2;
+beforeEach(async done => {
+  t1 = createTransport();
+  t2 = createTransport();
+  t1.on("negotiation", msg => t2.handleNegotiation(msg).catch(done.fail));
+  t2.on("negotiation", msg => t1.handleNegotiation(msg).catch(done.fail));
+  await t1.startNegotiation().catch(done.fail);
+
+  await Promise.all([
+    new Promise(r => t1.once("open", r)),
+    new Promise(r => t2.once("open", r))
+  ]);
+  d1 = t1.dataHandler;
+  d2 = t2.dataHandler;
+  done();
+});
+afterEach(() => {
+  t1.close();
+  t2.close();
+  t1 = t2 = null;
+  d1 = d2 = null;
+});
+
+describe("constructor()", () => {
+  it("should be closed: false", () => {
+    expect(d1.closed).toBeFalsy();
+    expect(d2.closed).toBeFalsy();
+  });
+});
+
+describe("close()", () => {
+  it("should be closed: true", () => {
+    d1.close();
+    expect(d1.closed).toBeTruthy();
+
+    d2.close();
+    expect(d2.closed).toBeTruthy();
+  });
+
+  it("should throw after closed", async () => {
+    d1.close();
+    await d1.createChannel().catch(err => {
+      expect(err).toMatch(/closed/);
+    });
+  });
+});
+
+describe("createChannel()", () => {
+  it("should create channel", async done => {
+    d2.once("channel", c2 => {
+      expect(c2 instanceof RTCDataChannel).toBeTruthy();
+      done();
+    });
+
+    const c1 = await d1.createChannel().catch(done.fail);
+    expect(c1 instanceof RTCDataChannel).toBeTruthy();
+  });
+
+  it("should specify label and dcInit", async done => {
+    d2.once("channel", c2 => {
+      expect(c2.label).toBe("hello");
+      expect(c2.ordered).toBeFalsy();
+      done();
+    });
+
+    const c1 = await d1
+      .createChannel("hello", { ordered: false })
+      .catch(done.fail);
+    expect(c1.label).toBe("hello");
+    expect(c1.ordered).toBeFalsy();
+  });
+});
