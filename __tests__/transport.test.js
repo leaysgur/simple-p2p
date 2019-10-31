d4e6403b65337272998fd8a168b6515a1b93b9e1
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-10-31 11:33:11 +0900

    Test

--- __tests__/transport.test.js
+++ __tests__/transport.test.js
@@ -196,3 +196,22 @@ describe("Transport#getStats()", () => {
     expect(stats instanceof RTCStatsReport).toBeTruthy();
   });
 });
+
+fdescribe("Transport#updateIceServers()", () => {
+  it("should update", done => {
+    const iceServers = [{ urls: "stun:stun.l.google.com:19302" }];
+
+    try {
+      t1.updateIceServers(iceServers);
+      t1.updateIceServers([]);
+      done();
+    } catch (err) {
+      if (/firefox/i.test(navigator.userAgent)) {
+        expect(err).toMatch("not support");
+        done();
+      } else {
+        done.fail("should not throw");
+      }
+    }
+  });
+});
