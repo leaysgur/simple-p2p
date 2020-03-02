811efd7d8fdffde9338bdbe3d2aa717353b2ae0e
Yuji Sugiura <y.sugiura.0316@gmail.com>
2020-03-02 10:13:07 +0900

    Update deps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import _debug from "debug";
-import Transport from "./transport";
-import DataHandler from "./data-handler";
-import MediaHandler from "./media-handler";
-import MediaSender from "./media-sender";
-import MediaReceiver from "./media-receiver";
+import _Transport from "./transport";
+import _DataHandler from "./data-handler";
+import _MediaHandler from "./media-handler";
+import _MediaSender from "./media-sender";
+import _MediaReceiver from "./media-receiver";
 
 const debug = _debug("simple-p2p");
 
@@ -17,11 +17,11 @@ export function createTransport(
   debug("create RTCPeerConnection w/", configuration);
   const pc = new RTCPeerConnection(configuration);
 
-  return new Transport(pc);
+  return new _Transport(pc);
 }
 
-export type Transport = InstanceType<typeof Transport>;
-export type DataHandler = InstanceType<typeof DataHandler>;
-export type MediaHandler = InstanceType<typeof MediaHandler>;
-export type MediaSender = InstanceType<typeof MediaSender>;
-export type MediaReceiver = InstanceType<typeof MediaReceiver>;
+export type Transport = InstanceType<typeof _Transport>;
+export type DataHandler = InstanceType<typeof _DataHandler>;
+export type MediaHandler = InstanceType<typeof _MediaHandler>;
+export type MediaSender = InstanceType<typeof _MediaSender>;
+export type MediaReceiver = InstanceType<typeof _MediaReceiver>;
